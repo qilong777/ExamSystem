3cc3a0598b8a85f28bed1658a52640a5466edef7
其龙4396 <5390236+dragon_4396@user.noreply.gitee.com>
2020-05-03 23:48:04 +0800

    5.3

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-// module.exports = {
-//   lintOnSave: false
-// }
+
 const path = require('path')// 引入path模块
 function resolve (dir) {
   return path.join(__dirname, dir)// path.join(__dirname)设置绝对路径
